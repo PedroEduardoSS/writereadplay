1e06bac48e799620b9945b291286064c22a6e8bd
PedroEduardoSS <pedroeduardossousa@gmail.com>
2025-02-13 19:16:31 -0300

    Add bulma css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css' }]
+    }
+  }
 })
